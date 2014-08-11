535ed3b4426385557e35557155ea616e0d9839ab
Johnson Qu <johnson.qu@sap.com>
2014-08-12 00:10:09 +0800

    add state machine to tender

--- app/models/tender.rb
+++ app/models/tender.rb
@@ -6,22 +6,53 @@ class Tender < ActiveRecord::Base
   has_many :bids
   has_one :bargain
   has_one :deal
+  has_one :deposit
 
   state_machine :initial => :intention do
-    before_transition :invite => any - :invite, :do => :check_user
+
+    before_transition :invite => any - :invite, :do => :check_deposit
+
     after_transition any => :closed do |tender, transition|
       # tender.noty_all
     end
+
     around_transition :log_transaction
 
-    event :initiate do
-      transition :intention => :invite
+    event :chose_subject do
+      transition :intention => :determined
     end
 
-    state :invite, :bargaining do
-      validates_presence_of :model
-      validates_presence_of :price
+    event :submit_margin do
+      transition :determined => :qualified
     end
+
+    event :invite_dealer do
+      transition :qualified => :invite
+    end
+
+    event :submit_tender do
+      transition [:invite, :bid_open] => :bid_open
+    end
+
+    event :submit_tender_closed do
+      transition :bid_open => :bid_closed
+    end
+
+    # user can make deal in the middle or end of the first round of bid
+    event :make_deal do
+      transition [:bid_open, :bid_closed] => :deal_closed
+    end
+
+    # if user not satisfied with result of first round, bargain
+    event :submit_bargain do
+      transition :bid_closed => :invite
+    end
+
+    state :determined do
+      validates_presence_of :model
+      # validates_presence_of :price
+    end
+
   end
 
   # before start tender valid user
@@ -31,7 +62,7 @@ class Tender < ActiveRecord::Base
 
   # before start check the deposit
   def check_deposit
-
+    self.deposit.present?
   end
 
   def log_transaction
