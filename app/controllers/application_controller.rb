55fe8070d58be26700c163d9144c99b528d211cd
Johnson Qu <johnson.qu@sap.com>
2014-08-25 13:39:39 +0800

    skip CSFR for json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  skip_before_filter :verify_authenticity_token, :if => :check_method
+
   before_action :set_locale
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+private
+
+  def check_method
+    request.format == :json
   end
 
 end
