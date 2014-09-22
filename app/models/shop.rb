b2580037fcc31d1b78eadf3e9a64dfac1d002906
Johnson Qu <johnson.qu@sap.com>
2014-09-22 22:15:48 +0800

    fix admin shop page

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,6 +2,6 @@ class Shop < ActiveRecord::Base
 
   has_and_belongs_to_many :tenders
   has_many :dealers, inverse_of: :shop
-  has_and_belongs_to_many :brands, :join_table => "brands_shops"
+  has_and_belongs_to_many :brands, join_table: "brands_shops", class_name: "Car::Brand"
 
 end
