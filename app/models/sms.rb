b46ae646780c2a2964b33ef642d198f9e8bf7a9f
Johnson Qu <johnson.qu@sap.com>
2014-11-01 15:24:52 +0800

    add sms noty new tender

--- app/models/sms.rb
+++ app/models/sms.rb
@@ -1,6 +1,5 @@
 class Sms
-  
-  
+
   def self.password(phone, password)
     company = "紫薯"
     template_value = CGI::escape "#code#=#{password}&#company#=#{company}"
@@ -12,8 +11,17 @@ class Sms
           "&key=b837cf1fbc2809288678f954c679495b"
     Typhoeus.post(url, body: {})
   end
-  
-  
-  
-  
+
+  def self.noty_new_tender(phone, tender)
+    content = tender.model.truncate(50)
+    template_value = CGI::escape "#content#=#{content}"
+    url = "http://v.juhe.cn"              <<
+          "/sms/send?"                    <<
+          "mobile=#{phone}"               <<
+          "&tpl_id=859"                   <<
+          "&tpl_value=#{template_value}"  <<
+          "&key=b837cf1fbc2809288678f954c679495b"
+    Typhoeus.post(url, body: {})
+  end
+
 end
