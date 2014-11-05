f8038b3ec48799a55ad3a0b44004681e4cc7f061
Johnson Qu <johnson.qu@sap.com>
2014-11-05 23:43:03 +0800

    typo

--- app/controllers/tenders_controller.rb
+++ app/controllers/tenders_controller.rb
@@ -110,10 +110,10 @@ class TendersController < InheritedResources::Base
   # }
   def create
     @tender = Tender.new(new_tender_params)
-    trim = Car::Trim.find(params[:tender][:trim_id])
-    @brand = trim.brand
-    @maker = trim.maker
-    @model = trim.model
+    @trim = Car::Trim.find(params[:tender][:trim_id])
+    @brand = @trim.brand
+    @maker = @trim.maker
+    @model = @trim.model
     colors = Car::Color.find(params[:tender][:colors_ids].split(','))
     @tender.model = "#{@brand.name} : #{@maker.name} : #{@model.name} : #{@trim.name} : #{colors.map(&:name).join(',')}"
     @tender.chose_subject!
