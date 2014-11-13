fdbdd07513519c292e0f919eaaf49f3d8a7783ab
jcheng7 <jackie.f.cheng@gmail.com>
2014-11-13 23:15:18 +0800

    add apk download

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,6 +19,12 @@ class HomeController < ApplicationController
   end
 
   def download
+    respond_to do |format|
+      format.html
+      format.json
+      format.png  { render :qrcode => root_url + '/pailixing.apk' }
+      format.jpeg { render :qrcode => root_url + '/pailixing.apk'}
+    end
   end
 
 end
