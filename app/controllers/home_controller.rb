be14d762376a6be3ef2237fc8cf4fb54d67fe658
jcheng7 <jackie.f.cheng@gmail.com>
2014-11-15 13:13:47 +0800

    add one more slogon on front page and change to 360download url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,8 +22,8 @@ class HomeController < ApplicationController
     respond_to do |format|
       format.html
       format.json
-      format.png  { render :qrcode => root_url + 'pailixing.apk' }
-      format.jpeg { render :qrcode => root_url + 'pailixing.apk'}
+      format.png  { render :qrcode => 'http://shouji.360tpcdn.com/141114/3e7a9e0064a93b68e2f486a4b6b5161a/com.cettco.buycar_1.apk' }
+      format.jpeg { render :qrcode => 'http://shouji.360tpcdn.com/141114/3e7a9e0064a93b68e2f486a4b6b5161a/com.cettco.buycar_1.apk'}
     end
   end
 
