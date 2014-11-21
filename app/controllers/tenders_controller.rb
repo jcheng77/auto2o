eecc2c36f536e17c50039d0680be8f3a43a7fa26
Johnson Qu <johnson.qu@sap.com>
2014-11-21 14:54:51 +0800

    add new super dealer

--- app/controllers/tenders_controller.rb
+++ app/controllers/tenders_controller.rb
@@ -21,7 +21,7 @@ class TendersController < InheritedResources::Base
 
   def dealer_index
     @dealer = current_dealer
-    if @dealer.shop.nil? and @dealer.phone == '18601207073'
+    if @dealer.shop.nil? && (@dealer.phone == '18601207073' || @dealer.phone == '18101880217')
     @tenders = Tender.where.not(state: %w(determined))
                       .includes(bargain: [bids:[:dealer]], deal: [dealer:[:shop]], car_trim: [model: [:pics]])
                       .order(id: :desc).page(params[:page]).per(10) 
